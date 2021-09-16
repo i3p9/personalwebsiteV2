a3aa7193ee2fa24251c633f4c5206bac20596097
i3p9 <i3p9@users.noreply.github.com>
2021-09-16 08:56:14 +0600

    fix: favicon, title and resume link fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,24 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.fahim.tech",
     title: "Fahim Faisal",
   },
   plugins: [
+    "gatsby-plugin-offline",
     "gatsby-plugin-use-dark-mode",
+    "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
+        name: "Fahim Faisal",
+        short_name: "Fahim",
+        start_url: "/",
         icon: "src/images/icon.png",
+        background_color: "#FFFFFF",
+        theme_color: "#FFFFFF",
+        display: "standalone",
+        theme_color_in_head: false,
       },
     },
     "gatsby-transformer-remark",
