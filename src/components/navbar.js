81a674f65c47697944306429cb4d648c744458e0
Fahim Faisal <i3p9@users.noreply.github.com>
2021-10-24 01:33:29 +0600

    fix: navbar flex, media fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,22 +5,32 @@ import useDarkMode from 'use-dark-mode'
 
 const Navbar = () => {
     const darkMode = useDarkMode(false);
+
     return(
-        <div className="container d-flex justify-content-between mt-5">
-            <nav>
-            <Link to="#" onClick={darkMode.toggle}> { darkMode.value ? 'light' : 'dark' } </Link>
-            </nav>
-            <nav>
-            <Link to="/" activeClassName="nav-active">home</Link>
-            {/* <Link to="/skills">skills</Link> */}
-            <Link to="/projects" activeClassName="nav-active">projects</Link>
-            <Link to="/fun" activeClassName="nav-active">fun</Link>
-            <a href="../../fahim_cv.pdf" target="_blank" class="resume-link">cv.pdf</a>
-            {/* <Link to="#" onClick={darkMode.toggle}> { darkMode.value ? 'light mode' : 'dark mode' } </Link> */}
-            </nav>
-            <script>
-            </script>
+        <div className="nav-flex" id="navbar">
+            <div className="darkmode-text">
+            <Link to="#" style={{textDecoration: "none"}} onClick={darkMode.toggle}> { darkMode.value ? 'light' : 'dark' } </Link>
+            </div>
+            <div className="nav-items">
+                <Link to="/" class="nav-item" activeClassName="nav-active">home</Link>
+                <Link to="/projects" class="nav-item" activeClassName="nav-active">projects</Link>
+                <Link to="/fun" class="nav-item" activeClassName="nav-active">fun</Link>
+                <a href="../../fahim_cv.pdf" class="nav-item">cv.pdf</a>
+            </div>
         </div>
+        // <div className="container d-flex justify-content-between mt-5">
+        // <nav>
+        // <Link to="#" onClick={darkMode.toggle}> { darkMode.value ? 'light' : 'dark' } </Link>
+        // </nav>
+        // <nav>
+        // <Link to="/" activeClassName="nav-active">home</Link>
+        // {/* <Link to="/skills">skills</Link> */}
+        // <Link to="/projects" activeClassName="nav-active">projects</Link>
+        // <Link to="/fun" activeClassName="nav-active">fun</Link>
+        // <a href="../../fahim_cv.pdf" target="_blank" class="resume-link">cv.pdf</a>
+        // {/* <Link to="#" onClick={darkMode.toggle}> { darkMode.value ? 'light mode' : 'dark mode' } </Link> */}
+        // </nav>
+        // </div>
     )
 }
 
