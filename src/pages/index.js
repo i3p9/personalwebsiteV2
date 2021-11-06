6e1a384f6dce927a84a45050b03ca7c89c378435
Fahim Faisal <i3p9@users.noreply.github.com>
2021-11-07 02:57:03 +0600

    fix: links on home and bg color

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,9 +43,9 @@ Currently I'm prepping for Master's and making cool <Link to="/projects" classNa
       </div>
       <div style={{marginTop: 15 + 'px', marginBottom: 50 + 'px'}}>
         <a href="mailto:fahimpfaisal@gmail.com" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={ faAt } /> fahimpfaisal@gmail.com</a>
-        <br/><a href="https://github.com/i3p9/" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={ faGithub } /> github</a>
-        <br/><a href="https://twitter.com/fahplusim" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={ faTwitter } /> twitter</a>
-        <br/><a href="https://www.last.fm/user/xfahim" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={ faLastfm } /> last.fm</a>
+        <br/><a href="https://github.com/i3p9/" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={ faGithub } /> i3p9</a>
+        <br/><a href="https://twitter.com/fahplusim" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={ faTwitter } /> fahplusim</a>
+        <br/><a href="https://www.last.fm/user/xfahim" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={ faLastfm } /> xfahim</a>
       </div>
     </div>
     </main>
