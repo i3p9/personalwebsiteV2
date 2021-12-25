31bbe09e8f9528b3582ab91965e4e3212183947f
i3p9 <i3p9@users.noreply.github.com>
2021-12-26 03:33:26 +0600

    fix: playback status better error handling

--- src/components/nowplaying.js
+++ src/components/nowplaying.js
@@ -7,7 +7,7 @@ config.autoAddCss = false
 const NowPlaying = () => {
     const [track, settrack] = useState("");
     const [artist, setartist] = useState("");
-    let isPlaying = false;
+    var isPlaying = "Last Played: ";
     const user = "xfahim"
     const apiKey = "6326961698ec80353b8b0f1118f76238"
     let callURL = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=" + user + "&api_key=" + apiKey + "&format=json"
@@ -17,17 +17,18 @@ const NowPlaying = () => {
         .then (data => {
             settrack(data.recenttracks.track[0].name)
             setartist(data.recenttracks.track[0].artist['#text'])
-            if (data.recenttracks.track[0]['@attr']['nowplaying']) {
+            if (data.recenttracks.track[0].hasOwnProperty('@attr')) {
                 console.log("In the playing thingy")
-                isPlaying = true;
+                isPlaying = "Now Playing: ";
                 console.log(isPlaying);
             }
+            console.log(isPlaying);
         })
     },[])
     console.log(track)
     return(
         <a href="https://www.last.fm/user/xfahim" class="now-playing-link" target="_blank" rel="noreferrer">
-        <div className="now-playing"><FontAwesomeIcon icon={ faSpotify } /> <span>{ isPlaying ? 'Last Played: ' : 'Now Playing: '} </span> { track } by { artist }</div>
+        <div className="now-playing"><FontAwesomeIcon icon={ faSpotify } /> <span>{ isPlaying } </span> { track } by { artist }</div>
         </a>
     )
 }
