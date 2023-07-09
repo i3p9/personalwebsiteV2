84e24d204da84fe194065f0e9fd4d13cca142afc
i3p9 <i3p9@users.noreply.github.com>
2023-07-10 03:12:06 +0600

    chore: logs, homepage bg fix?

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ import { faGithub, faTwitter, faLastfm } from '@fortawesome/free-brands-svg-icon
 
 const IndexPage = () => {
   return (
-    <main style={{ backgroundImage: `unset` }}>
+    <main>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Fahim Faisal</title>
@@ -32,22 +32,24 @@ const IndexPage = () => {
         <meta name="twitter:image" content="https://ffaisal.com/ogimage.webp" />
       </Helmet>
       <Navbar />
-      <div className="container">
-        <div>
-          <h1 className="home-name">Hey, I'm Fahim Faisal</h1>
-          I am a software engineer from Dhaka, Bangladesh. I like building things with Javascript and Python, and automating almost anything I can.
-          My hobbies include space, <a href="https://open.spotify.com/user/sphincone?si=eec6f56b1c9941a2" target="_blank" rel="noreferrer" style={{ textDecoration: "none", fontStyle: "italic" }} class="hover-underline-animation">making playlists</a> and taking pictures. I also like comedy shows, I'm a big fan of SNL.
-          Currently I'm working and making cool <Link to="/projects" className="hover-underline-animation" style={{ textDecoration: "none" }}>pet projects</Link>.
-          {/* <div className="new-line">Note: This site is still being built. You can visit my old site here: <a href="https://old.fahim.tech">https://old.fahim.tech</a></div> */}
-          <NowPlaying />
+      <body style={{ backgroundImage: `unset` }}>
+        <div className="container">
+          <div>
+            <h1 className="home-name">Hey, I'm Fahim Faisal</h1>
+            I am a software engineer from Dhaka, Bangladesh. I like building things with Javascript and Python, and automating almost anything I can.
+            My hobbies include space, <a href="https://open.spotify.com/user/sphincone?si=eec6f56b1c9941a2" target="_blank" rel="noreferrer" style={{ textDecoration: "none", fontStyle: "italic" }} class="hover-underline-animation">making playlists</a> and taking pictures. I also like comedy shows, I'm a big fan of SNL.
+            Currently I'm working and making cool <Link to="/projects" className="hover-underline-animation" style={{ textDecoration: "none" }}>pet projects</Link>.
+            {/* <div className="new-line">Note: This site is still being built. You can visit my old site here: <a href="https://old.fahim.tech">https://old.fahim.tech</a></div> */}
+            <NowPlaying />
+          </div>
+          <div style={{ marginTop: 5 + 'px', marginBottom: 50 + 'px' }}>
+            <a href="mailto:fahimpfaisal@gmail.com" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faAt} /> fahimpfaisal@gmail.com</a>
+            <br /><a href="https://github.com/i3p9/" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /> i3p9</a>
+            <br /><a href="https://twitter.com/fahplusim" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} /> fahplusim</a>
+            <br /><a href="https://www.last.fm/user/xfahim" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLastfm} /> xfahim</a>
+          </div>
         </div>
-        <div style={{ marginTop: 5 + 'px', marginBottom: 50 + 'px' }}>
-          <a href="mailto:fahimpfaisal@gmail.com" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faAt} /> fahimpfaisal@gmail.com</a>
-          <br /><a href="https://github.com/i3p9/" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /> i3p9</a>
-          <br /><a href="https://twitter.com/fahplusim" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} /> fahplusim</a>
-          <br /><a href="https://www.last.fm/user/xfahim" class="home-links" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLastfm} /> xfahim</a>
-        </div>
-      </div>
+      </body>
     </main>
   )
 }
